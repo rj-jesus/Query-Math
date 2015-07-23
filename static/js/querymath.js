53ba65161d2ed9f0a0555b63c77ecd62989d71c7
Ricardo Jesus <rj.bcjesus@gmail.com>
2015-07-23 19:51:34 +0100

    Improving user experience

--- static/js/querymath.js
+++ static/js/querymath.js
@@ -4,15 +4,9 @@ $(document).ready(function () {
         var input = $('#input-query').val();
         querymath(input);
     });
-    $('#btn-result').click(function () {
-        if ($('#result_text').is(':visible')) {
-            $('#btn-result').html('Show');
-            $('#result_text').hide();
-        }
-        else {
-            $('#btn-result').html('Hide');
-            $('#result_text').show();
-        }
+    $('#btn-result').click(function() {
+        $('#result-fix').hide();
+        $('#result').hide();
     });
 });
 
@@ -28,17 +22,16 @@ function querymath(query) {
             xmlDoc.async = false;
             xmlDoc.loadXML(data);
         }
-        console.log(xmlDoc);
         var text = '';
-        // console.log(xmlDoc.getElementsByTagName('pod')[0].getAttribute('title'));
         pod = xmlDoc.getElementsByTagName('pod');
         for (var i = 0; i < pod.length; i++) {
             text += '<h3>' + pod[i].getAttribute('title') + '</h3>\n';
             img = pod[i].getElementsByTagName('img');
             for (var j = 0; j < img.length; j++) text += '<img src=\"' + img[j].getAttribute('src') + '\" ' + 'alt=\"' + img[j].getAttribute('alt') + '"><br>';
         }
-        $('#result_text').html(text).show();
+        $('#result-fix').show();
+        window.scrollTo(0, document.getElementById('result-fix').offsetTop);
+        $('#result-text').html(text);
         $('#result').show();
-        $('#btn-result').html('Hide');
     });
 }
